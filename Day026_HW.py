8d0daa8e1aa70b2ab878c2c51afdcc909ffaa978
toby <toby19940927@gmail.com>
2020-02-24 19:02:45 +0800

    update d26

--- Day026_HW.py
+++ Day026_HW.py
@@ -7,5 +7,10 @@ class Day026HwSpider(scrapy.Spider):
     allowed_domains = ['https://www.ptt.cc']
     start_urls = ['https://www.ptt.cc/bbs/Gossiping/M.1582539066.A.C00.html']
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse, cookies={'over18': '1'})
+
     def parse(self, response):
+        print(response.text)
         pass
